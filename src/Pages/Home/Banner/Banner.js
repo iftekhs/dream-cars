f25cf52adf1d777b8cbfac34dcee473986b45c18
iftekhs <iftekhstudio@gmail.com>
2022-11-24 18:34:40 +0600

    changed the font to poppins

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import './banner.css';
 
 const Banner = () => {
-    return (
-        <section className='px-2 bg-emerald-500'>
-            
-        </section>
-    );
+  return (
+    <section className="px-2 bg-emerald-500">
+      <div className="banner flex items-center justify-center">
+        <div className="container mx-auto flex items-center justify-center">
+          <div>
+            <h1 className="text-5xl font-bold text-white">
+              Buy your dream car with the cheapest price!
+            </h1>
+            <p className="mt-2 text-lg text-white">
+              We resell exclusive cars with a very cheap price to help you get your dream car!
+              Currently we are offering a 10% discount on every car for the black friday!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../Layouts/Main';
+import Home from '../Pages/Home/Home';
 
 const router = createBrowserRouter([
   {
@@ -10,7 +11,7 @@ const router = createBrowserRouter([
         path: '/',
         element: (
           <>
-            <Main></Main>
+            <Home></Home>
           </>
         ),
       },
